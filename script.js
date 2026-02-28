e37502d382e49ba831a7829fdf7fbd746a48edf8
Lucas <lucasrian.dev@gmail.com>
2026-02-28 13:06:08 -0300

    começando js

---
+++ script.js
@@ -0,0 +1,20 @@
+const produtos = [
+  {
+    produto: "mouse",
+    src: "https://tse3.mm.bing.net/th/id/OIP.3rlO2nKgQRUVW8zR6i-SiAHaHa?pid=Api&P=0&h=180",
+    preco: "R$ 9,99",
+    id: 1,
+  },
+  {
+    produto: "teclado",
+    src: "https://cdn.shoppub.io/cdn-cgi/image/w=1000,h=1000,q=80,f=auto/oficinadosbits/media/uploads/produtos/foto/reuxysnw/file.png",
+    preco: "R$ 29,90",
+    id: 2,
+  },
+  {
+    produto: "notebook",
+    src: "https://netshopinformatica.com.br/uploads/product_commerce/products/4891/image/aapkd24053001utpf76-821d3e.jpg",
+    preco: "R$ 2,000",
+    id: 3,
+  },
+];
